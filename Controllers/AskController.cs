cca5b1870cdfb2baae9e62225669f9ebe37c71db
nthanhdev <nthanhdev@gmail.com>
2023-02-03 16:10:25 +0700

    valid

--- Controllers/AskController.cs
+++ Controllers/AskController.cs
@@ -14,29 +14,51 @@ namespace ChatChitApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Send(string prompt)
         {
-            HttpClient client = new HttpClient();
-
-            client.DefaultRequestHeaders.Add("authorization", "Bearer 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");
-
-            var content = new StringContent("{\"model\": \"text-davinci-003\", \"prompt\": \"" + prompt + "\",\"temperature\": 1,\"max_tokens\": 4000}",
-                Encoding.UTF8, "application/json");
-
-            HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
-            Console.WriteLine(response.IsSuccessStatusCode);
-            string responseString = await response.Content.ReadAsStringAsync();
-            
-            try
+            using (var client = new HttpClient())
             {
-                Models.AskResponse? askResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.AskResponse>(responseString); 
-                if(askResponse == null) return Ok("Lỗi !"); 
-                
-                return Ok(askResponse);
-            }catch(Exception e){
-                return Ok(e.Message);
+                // Add authorization header with API key
+                client.DefaultRequestHeaders.Add("authorization", "Bearer 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");
+                // Define request body with model, prompt, top_p, and max_tokens
+                var requestBody = new
+                {
+                    model = "text-davinci-003",
+                    prompt = prompt,
+                    top_p = 1,
+                    max_tokens = 4000
+                };
+                // Serialize request body to JSON and set as payload
+                var content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(requestBody),
+                    Encoding.UTF8, "application/json");
+
+                // Make post request to API
+                HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+                // Check if API response is successful
+                if (response.IsSuccessStatusCode == true)
+                {
+                    // Read response content as string
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    try
+                    {
+                        // Deserialize response string to Models.AskResponse object
+                        Models.AskResponse? askResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.AskResponse>(responseString);
+                        if (askResponse == null) return Ok("Lỗi !");
+
+                        // Return askResponse as response
+                        return Ok(askResponse);
+                    }
+                    catch (Exception e)
+                    {
+                        // Return error message if exception occurs during deserialization
+                        return Ok(e.Message);
+                    }
+                }
+
+
             }
-            finally{
-                client.Dispose();
-            }
+            // Return empty response if API response is not successful
+
+            return Ok();
 
         }
     }
